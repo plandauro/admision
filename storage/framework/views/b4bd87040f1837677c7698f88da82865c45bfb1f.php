cfbf68f0d6d230d68cf0d0040d7673385397cc54
grondanunab <grondan@unab.edu.pe>
2021-01-20 08:29:40 -0500

    cambiiofdds1

--- storage/framework/views/b4bd87040f1837677c7698f88da82865c45bfb1f.php
+++ storage/framework/views/b4bd87040f1837677c7698f88da82865c45bfb1f.php
@@ -1,45 +1,45 @@
 <?php $__env->startSection('title', 'RESULTADOS DEL EXAMEN GENERAL 2020-II'); ?>
 <?php $__env->startSection('content'); ?>
-  @parent
-  
-  <div class="">
-    <div class="row">
-      <div class="col-md-12 col-sm-12 col-xs-12">
-        <div class="x_panel">
-          <div class="x_title">
-            <h1>Reporte de Calificacion │ Examen General 2020-II</h1>
-                        <br>     <br>                   
-            <div class="col-md-2">
-			<button><a  href="<?php echo e(url('llenar-respuestas-2020-2')); ?>">Proceso de Calificación</a></button>
-		</div>
+@parent
 
-            <ul class="nav navbar-right panel_toolbox">
-              <li style="margin-right: 15px">
-                <h4> Filtros de busqueda:</h4>
-              </li>
-              <li style="margin-right: 15px">
-                <select style="width: 200px" id="tipobusqueda" onchange="cargarCombo()" class="form-control" name="" id="">
-                  <option value="0">Todos los postulantes</option>
-                  <option value="2">Por escuela</option>
-                </select>
-              </li>
-              <li>
-                <select id="dato" style="width: 150px" onchange="consultaProducto()" disabled="true" class="form-control" name="" id="">
-                  <option value="0">Todos</option>
-                </select>
-              </li>
-            </ul>
-            <div class="clearfix"></div>
+<div class="">
+  <div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <div class="x_panel">
+        <div class="x_title">
+          <h1>Reporte de Calificacion │ Examen General 2020-II</h1>
+          <br> <br>
+          <div class="col-md-2">
+            
           </div>
-          <div class="x_content">
-            <p> <strong id="message"></strong></p>
-              <table id="myTable" width="100%" class="table table-hover table-bordered">
-              </table>
-          </div>
+
+          <ul class="nav navbar-right panel_toolbox">
+            <li style="margin-right: 15px">
+              <h4> Filtros de busqueda:</h4>
+            </li>
+            <li style="margin-right: 15px">
+              <select style="width: 200px" id="tipobusqueda" onchange="cargarCombo()" class="form-control" name="" id="">
+                <option value="0">Todos los postulantes</option>
+                <option value="2">Por escuela</option>
+              </select>
+            </li>
+            <li>
+              <select id="dato" style="width: 150px" onchange="consultaProducto()" disabled="true" class="form-control" name="" id="">
+                <option value="0">Todos</option>
+              </select>
+            </li>
+          </ul>
+          <div class="clearfix"></div>
+        </div>
+        <div class="x_content">
+          <p> <strong id="message"></strong></p>
+          <table id="myTable" width="100%" class="table table-hover table-bordered">
+          </table>
         </div>
       </div>
     </div>
   </div>
+</div>
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('css'); ?>
@@ -48,39 +48,39 @@
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('js'); ?>
-  <script src="<?php echo e(asset('js/jquery.dataTables.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('js/dataTables.buttons.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('js/jszip.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('js/pdfmake.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('js/vfs_fonts.js')); ?>"></script>
-  <script src="<?php echo e(asset('js/buttons.html5.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('js/buttons.print.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('js/myjs/rep-postulante-calificacion-2020-2.js')); ?>"></script>
-  <script>
-  	$(document).ready(function(){ 
-	$('#ididentificacion').click(function() { 
-	
-	var nombre = $("#nombre").val(); 
-	
-	$.ajax({ 
-	type: "POST", 
-	url: 'rep-calificacion-2020-2', 
-	success: function(data) { 
-	if(data.existe==""){
+<script src="<?php echo e(asset('js/jquery.dataTables.min.js')); ?>"></script>
+<script src="<?php echo e(asset('js/dataTables.buttons.min.js')); ?>"></script>
+<script src="<?php echo e(asset('js/jszip.min.js')); ?>"></script>
+<script src="<?php echo e(asset('js/pdfmake.min.js')); ?>"></script>
+<script src="<?php echo e(asset('js/vfs_fonts.js')); ?>"></script>
+<script src="<?php echo e(asset('js/buttons.html5.min.js')); ?>"></script>
+<script src="<?php echo e(asset('js/buttons.print.min.js')); ?>"></script>
+<script src="<?php echo e(asset('js/myjs/rep-postulante-calificacion-2020-2.js')); ?>"></script>
+<script>
+  $(document).ready(function() {
+        $('#ididentificacion').click(function() {
 
-	        $("#alert1").show(0).delay(15000).hide(0);
-	        $("#message1").text("No existe el archivo ... Por favor subirlo el DDL"); 
-	        
-	}else{	
+            var nombre = $("#nombre").val();
 
-	        $("#alert1").show(0).delay(15000).hide(0);
-	        $("#message1").text("Se cargo correctamente"); 
-	}
-	window.location.href;
-	}	
-	} 
-	}); 
-	});
-  </script>
+            $.ajax({
+                type: "POST",
+                url: 'rep-calificacion-2020-2',
+                success: function(data) {
+                  if (data.existe == "") {
+
+                    $("#alert1").show(0).delay(15000).hide(0);
+                    $("#message1").text("No existe el archivo ... Por favor subirlo el DDL");
+
+                  } else {
+
+                    $("#alert1").show(0).delay(15000).hide(0);
+                    $("#message1").text("Se cargo correctamente");
+                  }
+                  window.location.href;
+                }
+              }
+            });
+        });
+</script>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
